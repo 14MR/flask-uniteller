7fae7307d6d18663539314910f3c5ded53a3cdb6
Timur Mustafin <mr14mr@yandex.ru>
2018-04-21 19:09:14 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""
+Python library for Uniteller API
+"""
+
+from setuptools import setup
+
+INSTALL_REQUIRES = [
+]
+
+
+setup(
+    name='flask-uniteller',
+    version='1.0',
+    description='Python library for Uniteller API',
+    url='https://github.com/14MR/flask-uniteller',
+)
